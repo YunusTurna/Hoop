15795148eec21147a691aaa6398fea9a52717917
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-09-07 11:21:25 +0300

    Basic platform mechanics

---
+++ Hoop/Assets/Scripts/BallScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] private float forcePower = 10f;
+    
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        rb.AddForce(Vector3.up * forcePower * Time.deltaTime , ForceMode.Impulse);
+    }
+    
+}

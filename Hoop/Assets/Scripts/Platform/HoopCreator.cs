0b6ac7a3a5562e758a0d867d45d8ed4878a29b17
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-09-08 15:11:56 +0300

    Some changes

--- Hoop/Assets/Scripts/Platform/HoopCreator.cs
+++ Hoop/Assets/Scripts/Platform/HoopCreator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HoopCreator : MonoBehaviour
 {
     [SerializeField] private GameObject[] hoops;
-    [SerializeField] private GameObject basePoint, lastHoopObject;
+    [SerializeField] private GameObject basePoint, lastHoopObject, firstObject;
     [SerializeField] private float hoopDistance = 2f;
     [SerializeField] private int numberOfHoops;
     private int randomNumber = 0;
@@ -27,6 +27,11 @@ public class HoopCreator : MonoBehaviour
                 basePoint.transform.position.z
             );
             randomNumber = Random.Range(0, hoops.Length);
+            if(i == 0)
+            {
+                GameObject lastHoop = Instantiate(firstObject, new Vector3(spawnPosition.x, spawnPosition.y + hoopDistance, spawnPosition.z), Quaternion.identity);
+                lastHoop.transform.parent = basePoint.transform;
+            }
             if (i != numberOfHoops - -1)
             {
                 GameObject newHoop = Instantiate(hoops[randomNumber], spawnPosition, Quaternion.identity);
